537caf2f9ce85d1ce732a56df8d64a1e0f7178dd
YHLee89 <younghoonlee.89@gmail.com>
2017-03-10 15:01:40 -0500

    file added

---
+++ twitter_credential.py
@@ -0,0 +1,9 @@
+# consumer_key = "SGS9ZMk1h0CvkfVJ6uvFZiZfv"
+# consumer_secret = "BEcNaMbQbaG7oDoZKT2yztyYa0haJwlqmz8SYr0O5CBa3zIhIf"
+# access_key = "212920273-pjmAKzWw23rkLinFFNfJE9A96uE4F2qB3wr1vr7n"
+# access_secret = "wBsz1Ze4xOAtNTWelk0NJcyI2GRnEK77qcWNwK1A4jyvy"
+
+# consumer_key = "WkmU1ZOLH64dV5HZDaKUL9QDc"
+# consumer_secret = "uDJVYICdbZ7JTsCEwB6JI8ms8tkWdJsKiMLIdZnvJLWVf3c1HB"
+# access_key = "833549298725961728-5Fvngkzt92PLyV1dpGeqNiN0FQ9vfFB"
+# access_secret = "Jstd8N24MxiaSVqDXl6aayPWtVBTBLPWTNb1pQYyyWiqd"
